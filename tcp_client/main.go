c939d63e70fcbde3103bb12e58d4e6c9f4a1237f
Plant-ree <58399956+Plant-ree@users.noreply.github.com>
2025-07-06 19:17:55 +0300

    Update main.go

--- tcp_client/main.go
+++ tcp_client/main.go
@@ -12,7 +12,7 @@ func main() {
 	//var ip, port string
 	//fmt.Println("Insert IP and port number: ")
 	//fmt.Scan(&ip, &port)
-	arguments := [2]string{"127.0.1.8", "8084"}
+	arguments := [2]string{"192.168.100.41", "8084"}
 	if len(arguments) == 1 {
 		fmt.Println("Please provide host:port.")
 		return
